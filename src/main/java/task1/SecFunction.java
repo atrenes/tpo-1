520fb57704893cf7f09fb6d6e1f00a319450bcac
atrenes <hanalainendm@gmail.com>
2023-02-09 18:37:49 +0300

    fix braces

--- src/main/java/task1/SecFunction.java
+++ src/main/java/task1/SecFunction.java
@@ -1,13 +1,23 @@
 package main.java.task1;
 
 public class SecFunction {
-    public static double sec(double x) {
-        if (x % Math.PI/2 == 0 && x % Math.PI != 0) {
+    public static double sec(double x, int n) {
+        if (n <= 0) return Double.NaN;
+        if (x % (Math.PI/2) == 0d && x % Math.PI != 0d) {
             return Double.NaN;
         }
 
+        double res = 1;
+        double sign = 1;
+        double fact = 1;
+        double pow = 1;
+        for (int i = 1; i < n; i++) {
+            sign = sign * -1;
+            fact = fact * (2 * i - 1) * (2 * i);
+            pow = pow * x * x;
+            res = res + sign * pow / fact;
+        }
 
-
-        return 0d;
+        return 1d/res;
     }
 }
