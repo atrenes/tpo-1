0584b6dc1f988403b4e79c026b07637091bc66bf
atrenes <hanalainendm@gmail.com>
2023-02-09 17:47:40 +0300

    sec(x) NaN x's

--- src/main/java/task1/SecFunction.java
+++ src/main/java/task1/SecFunction.java
@@ -2,6 +2,12 @@ package main.java.task1;
 
 public class SecFunction {
     public static double sec(double x) {
+        if (x % Math.PI/2 == 0 && x % Math.PI != 0) {
+            return Double.NaN;
+        }
+
+
+
         return 0d;
     }
 }
