7cbadc69ccadfd1a8f72f57d226f32d186adc221
atrenes <hanalainendm@gmail.com>
2023-02-09 18:51:51 +0300

    +=, *= etc

--- src/main/java/task1/SecFunction.java
+++ src/main/java/task1/SecFunction.java
@@ -1,21 +1,21 @@
 package main.java.task1;
 
 public class SecFunction {
-    public static double sec(double x, int n) {
-        if (n <= 0) return Double.NaN;
+    public static double sec(double x, int iter) {
+        if (iter <= 0) return Double.NaN;
         if (x % (Math.PI/2) == 0d && x % Math.PI != 0d) {
             return Double.NaN;
         }
 
-        double res = 1;
-        double sign = 1;
-        double fact = 1;
-        double pow = 1;
-        for (int i = 1; i < n; i++) {
-            sign = sign * -1;
-            fact = fact * (2 * i - 1) * (2 * i);
-            pow = pow * x * x;
-            res = res + sign * pow / fact;
+        double res = 1d;
+        double sign = 1d;
+        double fact = 1d;
+        double pow = 1d;
+        for (int i = 1; i < iter; i++) {
+            sign *= -1;
+            fact *= (2 * i - 1) * (2 * i);
+            pow *= x * x;
+            res += sign * pow / fact;
         }
 
         return 1d/res;
