60980f51a2e16d8f51946afa83ae2411238befd6
atrenes <hanalainendm@gmail.com>
2023-02-09 20:32:00 +0300

    fixed maven, started tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
-package main.java;
+import task1.SecFunction;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        return;
     }
 }
--- src/test/java/task1/SecFunctionTest.java
+++ src/test/java/task1/SecFunctionTest.java
@@ -1,5 +1,22 @@
 package task1;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SecFunctionTest {
+    double eps = 0.1;
+    int iter = 10;
+
+    @ParameterizedTest
+    @ValueSource(doubles = {Math.PI/2, -Math.PI/2, 5*Math.PI/2, -3*Math.PI/2})
+    public void restrictedXCheck(double value) {
+        Assertions.assertTrue(Double.isNaN(SecFunction.sec(value, iter)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {})
+    public void positiveCheck(double value) {
+
+    }
 }
